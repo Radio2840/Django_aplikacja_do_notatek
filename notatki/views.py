06ce831f566fb27bd18a18064f1f40ad1c7cbe2c
kinga <kinga.smolarek1@gmail.com>
2023-05-22 20:23:44 +0200

    Dodanie widoku dla stworzenia notatki wraz z formularzem

--- notatki/views.py
+++ notatki/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .form import NoteForm
+from .models import Note
 
-# Create your views here.
+
 
 
 def note_list(request):
     notes = Note.objects.all()
     return render(request, 'notes/note_list.html', {'notes': notes})
+
+
+def create_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.user = request.user
+            note.save()
+            return redirect('notes_list')
+    else:
+        form = NoteForm()
+    return render(request, 'notes/create_note.html', {'form': form})
