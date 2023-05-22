440aae8718379e4cb8f664e4067b604b54619959
kinga <kinga.smolarek1@gmail.com>
2023-05-22 20:21:55 +0200

    Dodanie widoku dla wszystkich notatek

--- notatki/views.py
+++ notatki/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def note_list(request):
+    notes = Note.objects.all()
+    return render(request, 'notes/note_list.html', {'notes': notes})
